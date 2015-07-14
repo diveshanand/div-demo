9cbd8d69b96a02aa11423b79640ad53ae741b9b0
Divesh Anand <divesh.anand@buzzyears.com>
2015-07-14 13:19:58 +0530

    doing some changes

--- hell.php
+++ hell.php
@@ -4,11 +4,11 @@
             <h1>
                 efdvjkmf
                 
-          
+          doing some changes in file  to learn git 
         </title>
     </head>
     
     <body>
-        <p>lfjvkjkmfmgvlmfvcdsjfhvj<a href="http://google.com">hell</a>
+        <p>lfjvkjkmfmgvlmfvcdsjfhvj<a href="http://yahoo.com">hell</a>
     </body>
 </html>
